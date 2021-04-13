18c68dd86cbdc3cd6e20e1ee7f63d49e32138799
fatih_yavuz <bu.fatih.yavuz@gmail.com>
2021-04-13 19:29:36 +0300

    First commit.

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace LessonBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+        public int tamsayi = 1;
+        [SerializeField] int tamsayi2 = 100;
+        [SerializeField] float ondalikSayi = 0.25f;
+        float ondalikSayi2 = 5.252525f;
+        bool dogruYanlis = true;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
