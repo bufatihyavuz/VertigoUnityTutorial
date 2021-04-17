3b6aa6f25fe5795bc64490e4fa3ab423db37d7e6
fatih_yavuz <bu.fatih.yavuz@gmail.com>
2021-04-17 13:17:20 +0300

    added rotate script to cube object.

---
+++ Assets/Scripts/Lessons/RotateScript.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class RotateScript : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1;
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+        }
+    }
+}
